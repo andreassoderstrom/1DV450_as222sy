5d0ae30a1249cc260c15664da178e9ff63a9941b
Andreas Soderstrom <as222sy@student.lnu.se>
2013-02-05 12:38:54 +0100

    testar

---
+++ rubylab/laboration/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laboration::Application.config.secret_token = 'd11f8d09fe329efbf1a6e4e06e8d4762c9ea8a9b70e41acc627723a450ea24060ff6a350c53bce8e36a0ee1aba3a5f1923ca5ee14b8f11b0627c7482db6477e1'
